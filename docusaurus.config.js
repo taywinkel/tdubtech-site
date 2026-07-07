f598b3f968dd4764f2e464c344c166a60109cbf5
taywinkel <taywinkel@gmail.com>
2026-07-06 21:40:15 -0600

    Tutorial testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'TdubTech',
-  tagline: 'Tutorials and projects from TdubTech featuring CODESYS',
+  tagline: 'Tutorials and projects from TdubTech',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
