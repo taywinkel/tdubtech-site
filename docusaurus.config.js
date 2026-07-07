f9dbb1df00d3c4d778845be8d077aa1725d91809
taywinkel <taywinkel@gmail.com>
2026-07-06 21:12:23 -0600

    Update homepage tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'TdubTech',
-  tagline: 'Tutorials and projects from TdubTech',
+  tagline: 'Tutorials and projects from TdubTech featuring CODESYS',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
